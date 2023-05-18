4d73ae2c4f71dc7c040c6ca834e9ddcd08b442c4
brian <btchan@telus.net>
2023-05-17 18:35:19 -0600

    added comments

--- yesterday.sh
+++ yesterday.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Get the full paths for this bash script 
+# and the folder containing this script.
 path=$(readlink -f -- "$0")
 location=${path%/*}
 
+# Activate the chessenv venv environment.
 activate="/chessenv/bin/activate"
 fullpathactivate="$location$activate"
 
 source $fullpathactivate
 
+# Run yesterday.py
 yesterday="/yesterday.py"
 fullpathyesterday="$location$yesterday"
 
